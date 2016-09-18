f4ddc1da5b97f5d803082ccfcb0cc513a3ea1fdf
joeb000 <j.b5286@gmail.com>
2016-09-18 17:30:42 -0400

    updating compile_solc script to handle contract constructor args

--- compile_solidity.sh
+++ compile_solidity.sh
@@ -21,40 +21,43 @@ solc --abi -o $WORKING_DIR/tmp $SOLCFILENAME
 #TODO - WORK IN PROGRESS
 ################################################
 name=`awk "/function ${CONTRACT_EXACT_NAME}\(/ {print}" $SOLCFILENAME`
-echo "name:"
-echo $name
 conregex="\((.*)\)"
 if [[ $name =~ $conregex ]];
   then conArgs=${BASH_REMATCH[1]};
 fi
 
-echo "con args"
-echo $conArgs
-
 #echo $conArgs | awk -F',' '{print $1}'
 argNumber=`echo $conArgs | awk -F',' '{ for(i = 1; i <= NF; i++) { print $i; max=$i } }' | wc -l`
-echo $argNumber
 for (( i = 1; i <= $argNumber; i++ )); do
-  echo "hello"
+  printf "Please enter Argument $i - "
   echo $conArgs | awk -F',' "{print \$$i}"
+  read anarg
+  if [[ "${#buildStr}" -gt "0" ]]; then
+      buildStr=$buildStr$anarg,
+      buildGasStr="$buildGasStr + pad64(web3.toHex($anarg)) "
+  fi
+  if [[ "${#buildStr}" -eq "0" ]]; then
+      buildStr=$anarg,
+      buildGasStr=" + pad64(web3.toHex($anarg)) "
+  fi
 done
 ################################################
 
-
 echo " " > $DEPLOY_SCRIPT
 echo " " >> $DEPLOY_SCRIPT
+echo " function pad64(str) { return ('0000000000000000000000000000000000000000000000000000000000000000' + str.substring(2, str.length)).slice(-64);} " >> $DEPLOY_SCRIPT
 printf "%s" "var ${CONTRACT_NAME}Contract = web3.eth.contract(" >> $DEPLOY_SCRIPT
 printf "%s" `cat $WORKING_DIR/tmp/$CONTRACT_NAME.abi` >> $DEPLOY_SCRIPT
 printf "%s" ");" >> $DEPLOY_SCRIPT
 echo " " >> $DEPLOY_SCRIPT
+printf "%s" "var deploybin = '`cat $WORKING_DIR/tmp/$CONTRACT_NAME.bin`'" >> $DEPLOY_SCRIPT
 echo " " >> $DEPLOY_SCRIPT
-printf "%s" "var gasEstimate = web3.eth.estimateGas({ from: web3.eth.accounts[0], data:'" >> $DEPLOY_SCRIPT
-printf "%s" `cat $WORKING_DIR/tmp/$CONTRACT_NAME.bin` >> $DEPLOY_SCRIPT
-printf "%s" "' })" >> $DEPLOY_SCRIPT
+printf "%s" "var gasEstimate = web3.eth.estimateGas({ from: web3.eth.accounts[0], data: deploybin" >> $DEPLOY_SCRIPT
+printf "%s" " $buildGasStr })" >> $DEPLOY_SCRIPT
 echo " " >> $DEPLOY_SCRIPT
 echo " " >> $DEPLOY_SCRIPT
 #TODO - add support for constructor
-printf "%s" "var ${CONTRACT_NAME} = ${CONTRACT_NAME}Contract.new({" >> $DEPLOY_SCRIPT
+printf "%s" "var ${CONTRACT_NAME} = ${CONTRACT_NAME}Contract.new($buildStr {" >> $DEPLOY_SCRIPT
 printf "%s" " from: web3.eth.accounts[0]," >> $DEPLOY_SCRIPT
 printf "%s" " data: '" >> $DEPLOY_SCRIPT
 printf "%s" `cat $WORKING_DIR/tmp/$CONTRACT_NAME.bin` >> $DEPLOY_SCRIPT
